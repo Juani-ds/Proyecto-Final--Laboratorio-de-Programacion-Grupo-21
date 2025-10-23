ad1e0aa38ca229ff800337097196cc08a76e81b1
Alaina1313 <alainasabrina13@gmail.com>
2025-10-23 20:06:49 -0300

    modelo lugar

--- src/modelo/Lugar.java
+++ src/modelo/Lugar.java
@@ -8,8 +8,73 @@ package modelo;
 
 /**
  *
- * @author  Alaina Reyes
+ * @author Juan Manuel, Nerina, Nahuel, Alaina, Juan Ignacio, Tiziana
  */
 public class Lugar {
+    
+    private int codLugar;
+    private Proyeccion proyeccion;
+    private char fila;
+    private int numero;
+    private String estado;
 
+    public Lugar() {
+        this.estado = "Disponible";
+    }
+
+    public Lugar(int codLugar, Proyeccion proyeccion, char fila, int numero, String estado) {
+        this.codLugar = codLugar;
+        this.proyeccion = proyeccion;
+        this.fila = fila;
+        this.numero = numero;
+        this.estado = estado;
+    }
+
+    public int getCodLugar() {
+        return codLugar;
+    }
+
+    public void setCodLugar(int codLugar) {
+        this.codLugar = codLugar;
+    }
+
+    public Proyeccion getProyeccion() {
+        return proyeccion;
+    }
+
+    public void setProyeccion(Proyeccion proyeccion) {
+        this.proyeccion = proyeccion;
+    }
+
+    public char getFila() {
+        return fila;
+    }
+
+    public void setFila(char fila) {
+        this.fila = fila;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Lugar{" + "codLugar=" + codLugar + ", proyeccion=" + proyeccion + ", fila=" + fila + ", numero=" + numero + ", estado=" + estado + '}';
+    }
+    
+    
+    
 }
