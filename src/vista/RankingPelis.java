4412fd959c00a9c77b0ce9e8b765756075ef9926
Juan Ignacio Genero <juanignaciogenero@gmail.com>
2025-11-19 00:12:06 -0300

    Correccion pelismasvistas

--- src/vista/RankingPelis.java
+++ src/vista/RankingPelis.java
@@ -74,13 +74,13 @@ public class RankingPelis extends javax.swing.JInternalFrame {
 
         jTableRank.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Pelicula", "Espectadores", "Ingresos"
             }
         ));
         jScrollPane1.setViewportView(jTableRank);
